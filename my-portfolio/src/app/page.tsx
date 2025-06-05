a444d71a0316cfed94e11c0c1441d47509bc1217
Noel George C J <noelgeorge2004@gmail.com>
2025-06-05 22:01:32 +0530

    Configuration

---
+++ my-portfolio/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1 className="font-revelia">DIGITAL CRAFTSMAN</h1>
+    </div>
+  );
+}
